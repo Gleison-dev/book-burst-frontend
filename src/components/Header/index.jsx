36e99c96a90cfc10b45474313d337d669d52f225
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-02 10:50:18 -0300

    feat: adding logo on header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import logo from "../../assets/logo.svg"
+
+export default function Header() {
+  return (
+    <>
+      <section>
+        <div>
+          <div>
+            <img src={logo} alt="Logo do Book Burst" />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
