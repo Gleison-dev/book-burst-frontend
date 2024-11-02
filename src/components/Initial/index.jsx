2a3aafc3874f8108e6c005dd07f3164a051a53af
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-02 17:48:52 -0300

    feat: adding phrase on initial

--- src/components/Initial/index.jsx
+++ src/components/Initial/index.jsx
@@ -1,7 +1,16 @@
 export default function Initial() {
   return (
     <>
-      <section></section>
+      <section>
+        <div>
+          <h1 className="text-white">
+            Venha fazer parte da melhor <br />
+            plataforma de livros <br />
+            totalmente gratuita, conheça <br />
+            o Book Burst.
+          </h1>
+        </div>
+      </section>
     </>
-  )
+  );
 }
