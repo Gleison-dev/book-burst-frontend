64b949534db33dbdd8b8f35ac1b48c4af3220f16
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-02 11:01:13 -0300

    feat: header created and stylized

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,12 @@ export default function Header() {
   return (
     <>
       <section>
-        <div>
+        <div className="flex justify-center h-40 items-center bg-branco">
           <div>
             <img src={logo} alt="Logo do Book Burst" />
           </div>
         </div>
+        <hr className=" border-4 border-roxo"/>
       </section>
     </>
   )
