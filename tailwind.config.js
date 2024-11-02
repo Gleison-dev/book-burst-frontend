3bf7a0601383de1ecc95c39e155952cb7638c2a3
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-02 10:39:09 -0300

    feat: configuring tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        preto: "#28262C",
+        roxo: "#998FC7",
+        roxo_claro: "#D4C2FC",
+        branco: "#F9F5FF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
