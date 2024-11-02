9a09122d637e621e420f0ec8ecf11087dd8b2fd3
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-02 18:03:09 -0300

    feat: stlyzed initial page

--- src/components/Initial/index.jsx
+++ src/components/Initial/index.jsx
@@ -1,20 +1,24 @@
 import Button from "../Button";
+import reading_book from "../../assets/reading_book.svg"
 
 export default function Initial() {
   return (
     <>
-      <section>
+      <section className="flex justify-around items-center mt-36">
         <div>
-          <h1 className="text-white">
+          <h1 className="text-white font-merriweather text-3xl space-y-2">
             Venha fazer parte da melhor <br />
             plataforma de livros <br />
             totalmente gratuita, conheça <br />
-            o Book Burst.
+            o Book Burst. <br />
           </h1>
-          <div>
+          <div className="mt-10">
             <Button text="Consultar livros"/>
             <Button text="Cadastrar livro"/>
           </div>
+        </div>
+        <div>
+          <img className="w-96" src={reading_book} alt="Imagem de pessoas lendo livro" />
         </div>
       </section>
     </>
