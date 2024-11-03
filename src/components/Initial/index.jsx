077f257a83c3e2aec14bcc541c8b470aea8c107a
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-03 18:19:01 -0300

    feat: adding space and text 4xl

--- src/components/Initial/index.jsx
+++ src/components/Initial/index.jsx
@@ -6,13 +6,13 @@ export default function Initial() {
     <>
       <section className="flex justify-around items-center mt-36">
         <div>
-          <h1 className="text-white font-merriweather text-3xl space-y-2">
+          <h1 className="text-white font-merriweather text-4xl space-y-3">
             Venha fazer parte da melhor <br />
             plataforma de livros <br />
             totalmente gratuita, conheça <br />
             o Book Burst. <br />
           </h1>
-          <div className="mt-10">
+          <div className="mt-20">
             <Button text="Consultar livros"/>
             <Button text="Cadastrar livro"/>
           </div>
