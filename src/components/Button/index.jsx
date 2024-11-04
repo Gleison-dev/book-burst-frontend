67a471c41337640a51960721cec8899dda4c466f
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-04 11:05:56 -0300

    feat: adding hover on button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 export default function Button({ text }) {
   return (
     <>
-      <button className="w-52 h-20 bg-roxo text-white rounded-2xl mr-5 font-roboto_mono">{text}</button>
+      <button className="w-52 h-20 bg-roxo text-white rounded-2xl mr-5 font-roboto_mono hover:bg-roxo_claro">{text}</button>
     </>
   );
 }
