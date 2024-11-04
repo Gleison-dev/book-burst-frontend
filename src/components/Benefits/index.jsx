b1c2551d83835c07d82d50589d7e45594fa84843
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-04 19:13:37 -0300

    feat: benefits stlyzed

--- src/components/Benefits/index.jsx
+++ src/components/Benefits/index.jsx
@@ -1,16 +1,33 @@
 import Circles from "../Circles";
+import icon_books from "../../assets/icon_books.svg";
+import icon_ideia from "../../assets/icon_ideia.svg";
+import icon_library from "../../assets/icon_library.svg";
 
 export default function Benefits() {
   return (
     <>
-      <section>
-        <div>
-          <h2>O que nós te oferecemos:</h2>
+      <section className="bg-branco">
+        <div className="flex justify-center">
+          <h2 className="text-2xl">O que nós te oferecemos:</h2>
         </div>
-        <div>
-          <Circles />
+        <div className="mt-20 flex justify-around items-center">
+          <Circles
+            img={icon_books}
+            alt="Icóne de livros"
+            text="Livros totalmente gratuitos"
+          />
+          <Circles
+            img={icon_library}
+            alt="Icóne de catálogo"
+            text="Catálogo gigante"
+          />
+          <Circles
+            img={icon_ideia}
+            alt="Icóne de intuitivo"
+            text="Plataforma intuitiva"
+          />
         </div>
       </section>
     </>
-  )
+  );
 }
--- src/components/Circles/index.jsx
+++ src/components/Circles/index.jsx
@@ -1,12 +1,12 @@
 export default function Circles({ img, alt, text }) {
   return (
     <>
-      <div>
-        <div>
-          <img src={img} alt={alt} />
+      <div className="flex justify-center items-center flex-col">
+        <div className="border-4 border-roxo_claro rounded-full w-48 p-4">
+          <img className="w-36" src={img} alt={alt} />
         </div>
-        <div>
-          <p>{text}</p>
+        <div className="mt-5">
+          <p className="text-xl">{text}</p>
         </div>
       </div>
     </>
