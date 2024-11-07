e4945ec166023b8a82937568f1be799c2c672c83
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-07 18:51:30 -0300

    feat: stylzed register page

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -4,35 +4,35 @@ import Button from "../Button/index";
 export default function Register() {
   return (
     <>
-      <section className="text-white">
+      <section className="flex justify-center items-center h-screen flex-col text-white">
         <div>
-          <h1 className="font-playfair">Cadastrar livro</h1>
+          <h1 className="font-playfair text-5xl underline">Cadastrar livro</h1>
         </div>
-        <form>
-          <label>Título:</label> <br />
-          <input type="text" placeholder="digite o título do livro" /> <br />
-          <label>Escritor(a):</label> <br />
-          <input
+        <form className="flex justify-around items-center flex-col font-roboto_mono mt-32 border p-8 rounded-md">
+          <label className="mt-10 text-xl">Título:</label> <br />
+          <input className="bg-preto border-b-2 w-64 outline-none border-roxo_claro" type="text" placeholder="digite o título do livro" /> <br />
+          <label className="mt-10 text-xl">Escritor(a):</label> <br />
+          <input className="bg-preto border-b-2 w-64 outline-none border-roxo_claro"
             type="text"
             placeholder="digite o nome completo do autor(a)"
           />{" "}
           <br />
-          <label>Gênero:</label> <br />
-          <input type="text" placeholder="insira o gênero do livro" /> <br />
-          <label>Ano de lançamento:</label> <br />
-          <input type="number" placeholder="insira o ano de lançamento" />{" "}
+          <label className="mt-10 text-xl">Gênero:</label> <br />
+          <input className="bg-preto border-b-2 w-64 outline-none border-roxo_claro" type="text" placeholder="insira o gênero do livro" /> <br />
+          <label className="mt-10 text-xl">Ano de lançamento:</label> <br />
+          <input className="bg-preto border-b-2 w-64 outline-none border-roxo_claro" type="number" placeholder="insira o ano de lançamento" />{" "}
           <br />
-          <label>Link:</label> <br />
-          <input
+          <label className="mt-10 text-xl">Link:</label> <br />
+          <input className="bg-preto border-b-2 w-64 outline-none border-roxo_claro"
             type="text"
             placeholder="insira o link de download do livro"
           />{" "}
           <br />
-          <div>
+          <div className="mt-20">
             <Link to="/">
-              <Button text="Voltar"/>
+              <Button text="Voltar" />
             </Link>
-            <Button text="Cadastrar"/>
+            <Button text="Cadastrar" />
           </div>
         </form>
       </section>
