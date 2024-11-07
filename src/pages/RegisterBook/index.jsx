2048077d01730bfe7fc75e0fa7d2fdbab393bf08
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-07 10:06:05 -0300

    feat: register file created and imported on register book page

--- src/pages/RegisterBook/index.jsx
+++ src/pages/RegisterBook/index.jsx
@@ -1,10 +1,14 @@
 import Header from "../../components/Header/index"
+import Register from "../../components/Register"
 
 export default function RegisterBook() {
   return (
     <>
       <section>
         <Header />
+      </section>
+      <section>
+        <Register />
       </section>
     </>
   )
