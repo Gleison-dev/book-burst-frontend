75979652dd4d3900a2416394ce249013b58a9929
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-07 18:36:00 -0300

    feat: adding button on register

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import Button from "../Button/index";
+
 export default function Register() {
   return (
     <>
@@ -7,17 +10,32 @@ export default function Register() {
         </div>
         <form>
           <label>Título:</label> <br />
-          <input type="text" placeholder="digite o título do livro"/> <br />
+          <input type="text" placeholder="digite o título do livro" /> <br />
           <label>Escritor(a):</label> <br />
-          <input type="text" placeholder="digite o nome completo do autor(a)"/> <br />
+          <input
+            type="text"
+            placeholder="digite o nome completo do autor(a)"
+          />{" "}
+          <br />
           <label>Gênero:</label> <br />
-          <input type="text" placeholder="insira o gênero do livro"/> <br />
+          <input type="text" placeholder="insira o gênero do livro" /> <br />
           <label>Ano de lançamento:</label> <br />
-          <input type="number" placeholder="insira o ano de lançamento"/> <br />
+          <input type="number" placeholder="insira o ano de lançamento" />{" "}
+          <br />
           <label>Link:</label> <br />
-          <input type="text" placeholder="insira o link de download do livro"/> <br />
+          <input
+            type="text"
+            placeholder="insira o link de download do livro"
+          />{" "}
+          <br />
+          <div>
+            <Link to="/">
+              <Button text="Voltar"/>
+            </Link>
+            <Button text="Cadastrar"/>
+          </div>
         </form>
       </section>
     </>
-  )
+  );
 }
