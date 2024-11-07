bb7e6689e3b739fc0120d70b2f86c6c21c5b52a0
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-11-07 10:14:54 -0300

    feat: schedule created

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,7 +1,23 @@
 export default function Register() {
   return (
     <>
-      <section></section>
+      <section className="text-white">
+        <div>
+          <h1 className="font-playfair">Cadastrar livro</h1>
+        </div>
+        <form>
+          <label>Título:</label> <br />
+          <input type="text" placeholder="digite o título do livro"/> <br />
+          <label>Escritor(a):</label> <br />
+          <input type="text" placeholder="digite o nome completo do autor(a)"/> <br />
+          <label>Gênero:</label> <br />
+          <input type="text" placeholder="insira o gênero do livro"/> <br />
+          <label>Ano de lançamento:</label> <br />
+          <input type="number" placeholder="insira o ano de lançamento"/> <br />
+          <label>Link:</label> <br />
+          <input type="text" placeholder="insira o link de download do livro"/> <br />
+        </form>
+      </section>
     </>
   )
 }
